3a579c768e68f338c95d09e370d611b8403addf7
sheringugi <sheri.ngugi17@gmail.com>
2023-03-23 01:46:08 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) =>{
+    e.preventDefault()
+    console.log(e.target)
+  })
+  
 });
